52895195b114fafe274baf22aa589a307801c092
jluethi <joel.luethi@uzh.ch>
2024-08-08 13:38:24 +0200

    Update fractal-tasks template & update to pydantic >2

--- src/fractal_helper_tasks/__init__.py
+++ src/fractal_helper_tasks/__init__.py
@@ -1,3 +1,8 @@
-"""
-Collection of Fractal helper tasks
-"""
+"""Collection of Fractal helper tasks"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("fractal-helper-tasks")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
--- src/fractal_helper_tasks/dev/create_manifest.py
+++ src/fractal_helper_tasks/dev/create_manifest.py
@@ -1,7 +1,10 @@
-"""Generate JSON schemas for tasks and write them to the Fractal manifest."""
+"""Manifest generation"""
 
 from fractal_tasks_core.dev.create_manifest import create_manifest
 
 if __name__ == "__main__":
+    """Generate JSON schemas for task arguments afresh, and write them
+    to the package manifest.
+    """
     PACKAGE = "fractal_helper_tasks"
     create_manifest(package=PACKAGE)

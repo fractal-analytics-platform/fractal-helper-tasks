a11fe7819e07427e9933c47425bab9994933d9ea
jluethi <joel.luethi@uzh.ch>
2026-06-08 12:18:36 +0200

    Update ngio API calls for add_z_singleton

--- src/fractal_helper_tasks/convert_2D_segmentation_to_3D.py
+++ src/fractal_helper_tasks/convert_2D_segmentation_to_3D.py
@@ -149,7 +149,6 @@ def convert_2D_segmentation_to_3D(
 
         z_extent = nb_z_planes * pixel_size.z
 
-        print(pixel_size)
         new_label_container = ome_zarr_container_3d.derive_label(
             name=new_label_name,
             ref_image=ref_image_3d,
